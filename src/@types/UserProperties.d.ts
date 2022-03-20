b1535fee2140aaaf78c0807c3640651fe8530ca8
Michael Castillo Granados <michaelgranados@gmail.com>
2022-03-19 21:41:24 -0300

    ✨ google script created

---
+++ src/@types/UserProperties.d.ts
@@ -0,0 +1,4 @@
+declare namespace UserProperties {
+  function getProperty(key: string): any
+  function setProperty(key: string, value: any): void
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-export default {}
+/**
+ * get api key for user
+ */
+function getApiKey(): any {
+  return UserProperties.getProperty('bling_api_key')
+}
+
+/**
+ * set api key for user
+ *
+ * @param apiKey string api key to set to user
+ */
+function setApiKey(apiKey: string): void {
+  UserProperties.setProperty('bling_api_key', apiKey)
+}
